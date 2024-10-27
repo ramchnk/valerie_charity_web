60c9694eae03557af76107cc32e71a06c6902726
rama chandran <ramachandran@MacBook-Air.local>
2024-10-27 09:18:27 +0530

    test php changes

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,28 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+// Replace with your email address
+$recipient = "ramchnk@gmail.com";
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+// Getting form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+// Construct the email content
+$email_content = "Name: $name\n";
+$email_content .= "Email: $email\n";
+$email_content .= "Subject: $subject\n\n";
+$email_content .= "Message:\n$message\n";
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+// Construct the email headers
+$headers = "From: $email";
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+// Send the email
+if (mail($recipient, $subject, $email_content, $headers)) {
+    http_response_code(200);
+    echo "Thank You! Your message has been sent.";
+} else {
+    http_response_code(500);
+    echo "Oops! Something went wrong, and we couldn't send your message.";
+}
 ?>
